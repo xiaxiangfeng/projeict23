49de46f6d586f5aac5389e5e526cc2802a529912
xxf <876029169@qq.com>
2023-11-08 22:27:21 +0800

    chat

---
+++ docs/chat/layouts__index.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[717],{52756:function(l,t,n){n.r(t),n.d(t,{default:function(){return P}});var E=n(46998),s=n(32896),O=n(20563),u=n(85893);function P(){return(0,u.jsx)(s.ZP,{theme:{token:{colorPrimary:"#615ced"}},children:(0,u.jsx)(E.Outlet,{})})}}}]);
